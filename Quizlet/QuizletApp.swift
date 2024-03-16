a93e5710f22641643d3f0f13b2e523b1025d39c0
danila.bondarenko <danila.bondarenko@nordclan.com>
2024-03-16 19:26:48 +0300

    Initial Commit

---
+++ Quizlet/QuizletApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizletApp.swift
+//  Quizlet
+//
+//  Created by Данила Бондаренко on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuizletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
