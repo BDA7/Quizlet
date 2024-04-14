c6536cb34ebf9e76c38062618a87004a8c5a06d5
danila.bondarenko <danila.bondarenko@nordclan.com>
2024-04-14 20:50:26 +0300

    add realm service to middleware

---
+++ Quizlet/Quizlet/Middlewares/QuestionsMiddleware.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionsMiddleware.swift
+//  Quizlet
+//
+//  Created by Данила Бондаренко on 14.04.2024.
+//
+
+import Foundation
---
+++ Quizlet/Quizlet/UI/HomeView/View/ChapterGrid.swift
@@ -0,0 +1,18 @@
+//
+//  ChapterGrid.swift
+//  Quizlet
+//
+//  Created by Данила Бондаренко on 14.04.2024.
+//
+
+import SwiftUI
+
+struct ChapterGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChapterGrid()
+}
